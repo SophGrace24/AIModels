fcbb8d970db9136240042e17c1a0842d48252598
Sophia Woods <191865453+SophGrace24@users.noreply.github.com>
2025-06-02 06:15:27 -0400

    Fixing format

--- tests/test_UniqueMetric.py
+++ tests/test_UniqueMetric.py
@@ -1,25 +1,32 @@
 import unittest
 from Unique.Unique_Metric_Base import generateBalancedProfile
 
-"""
-test_UniqueMetric.py
-This file is specific for the Unique Metric - generate balanced profile function.
-"""
 
 class TestGenerateBalancedProfile(unittest.TestCase):
     def test_basicCalculations(self):
-    dimensions= ['clarity', 'empathy','novelty']
-    profileA = {
+        dimensionns = ['clarity', 'empathy','novelty']
+
+        profileA = {
         'clarity': 0.7,
         'empathy': 0.04,
         'novelty': 0.4
-    }
-    profileB =  {
+        }
+
+        profileB =  {
         'clarity': 0.4,
         'empathy': 0.8,
         'novelty': 0.5
     }
-    WeightA = (0.60)
+
+
+    WeightA = (0.6)
     WeightB = 1 - WeightA
 
-    pass
+    # minScore and maxScore will use defaults 0.0 and 1.0 for this test
+    # expect no clipping with these input scores
+
+    expected_BalancedProfile = (
+        'clarity': (0.6 * 0.7) + (0.4 * 0.4) = 0.580
+        'empathy': (0.6 * 0.04) + (0.4 * 0.8) = 0.344
+        'novelty': {0.6 * 0.4} + (0.4 * 0.5) = 0.440
+    )
